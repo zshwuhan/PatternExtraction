79cbedbdd51a58aa9cd9e583f3eaab18bfe28fa9
AsteriskAmpersand <aravuegnitsuga@gmail.com>
2015-02-25 00:45:16 -0500

    Minor Changes
    -Removed previous excecutable version
    -Added a command prompt that opens on directory for commodity

---
+++ cmd.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cmd.exe "cd C:\Users\Agustin\Documents\GitHub\PatternExtraction && C: && g++ main.cpp -o prefixSpan.exe -static-libgcc -static-libstdc++ -std=c++11 -Wall sequence_hash.cpp helperFunctions.cpp seq_pointer.cpp prefixSpan.cpp"
+
